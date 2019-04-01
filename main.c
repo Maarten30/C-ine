8c59817771b087246754f9d6223aeda909c4d564
Maarten <maartenhandels@opendeusto.es>
2019-04-01 23:21:35 +0200

    Avances menu Principal

--- main.c
+++ main.c
@@ -5,9 +5,32 @@
  *      Author: maarten
  */
 #include <stdio.h>
+#include "gestor.h"
+
 
 int main(void)
 {
 	printf("Hola mundo");
+
+	char caracter;
+
+	do
+	{
+		printf("Menu Principal: \n");
+		printf("1.- Cartelera\n2.- Compra de entradas\n");
+		scanf(" %c", &caracter);
+		printf("%c\n", caracter);
+
+
+		if(caracter == 'g')
+		{
+			menuGestor();
+		}
+
+
+	}while(caracter!='q');
+
 }
+
+
 
